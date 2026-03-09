bb406a8546a47cdf23930c3bdb38386148279d33
kparpinelli24-sys <k.parpinelli.24@gmail.com>
2026-03-09 13:07:58 -0300

    autor

--- src/routes/musicaRoutes.js
+++ src/routes/musicaRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router()
 const MusicaController = require('../controllers/musicaControllers');
 
-router.get('/', MusicaController.listarTodos);
+router.get('/ todos/:todos', MusicaController.listarTodos);
 router.get('/autor/:autor', MusicaController.buscarPorAutor);
 router.get('/:id', MusicaController.buscarPorId);
 router.post('/', MusicaController.criar);
