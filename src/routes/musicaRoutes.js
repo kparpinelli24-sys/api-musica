c29d2661b23b2fc1f7864f07bf3b07797ab19b4a
kparpinelli24-sys <k.parpinelli.24@gmail.com>
2026-03-09 13:09:35 -0300

    nome musica modificado

--- src/routes/musicaRoutes.js
+++ src/routes/musicaRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router()
 const MusicaController = require('../controllers/musicaControllers');
 
-router.get('/ todos/:todos', MusicaController.listarTodos);
-router.get('/autor/:autor', MusicaController.buscarPorAutor);
+router.get('/', MusicaController.listarTodos);
+router.get('/nomemusica', MusicaController.buscarPorAutor);
 router.get('/:id', MusicaController.buscarPorId);
 router.post('/', MusicaController.criar);
 router.put('/:id', MusicaController.atualizar);
